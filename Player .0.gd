616537971025e52a9b172385530e050df77f6904
Garrett <g2.trouble11@gmail.com>
2024-05-23 20:44:23 -0700

    Movement Working!!!!!

---
+++ Player .0.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+const SPEED = 300
+
+
+func _ready():
+	pass
+	
+	
+	
+func _process(delta):
+	if Input.is_action_pressed("W"):
+		velocity.y = -SPEED
+	elif Input.is_action_pressed("S"):
+		velocity.y = SPEED
+	else:
+		velocity.y = 0
+		
+	if Input.is_action_pressed("D"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("A"):
+		velocity.x = -SPEED
+	else:
+		velocity.x = 0
+
+	move_and_slide()
