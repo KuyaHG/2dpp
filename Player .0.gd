cdcdc5b0c9485ddcaa685ca4e7daf79a788e258f
Herschel Murray <herschelgmurray@gmail.com>
2024-05-28 22:06:49 -0700

    Still kinda confused on what's going on, velocity is not working GARREETTT

--- Player .0.gd
+++ Player .0.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
-const SPEED = 300
-
+const SPEED = 3
+var screen_size
 
 func _ready():
-	pass
-	
-	
-	
-func _process(_delta):
+	screen_size = get_viewport_rect().size
+	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+
+func _physics_process(delta):
+	# var velocity = Vector2.ZERO
+
 	if Input.is_action_pressed("W"):
 		velocity.y = -SPEED
 	elif Input.is_action_pressed("S"):
@@ -22,5 +23,32 @@ func _process(_delta):
 		velocity.x = -SPEED
 	else:
 		velocity.x = 0
-
+		
+	velocity *= delta
+	
 	move_and_slide()
+
+#func _process(delta):
+	#var velocity = Vector2.ZERO
+#
+	#if Input.is_action_pressed("W"):
+		#velocity.y = -SPEED
+	#elif Input.is_action_pressed("S"):
+		#velocity.y = SPEED
+	#else:
+		#velocity.y = 0
+		#
+	#if Input.is_action_pressed("D"):
+		#velocity.x = SPEED
+	#elif Input.is_action_pressed("A"):
+		#velocity.x = -SPEED
+	#else:
+		#velocity.x = 0
+	#
+	#var new_velocity = velocity * delta
+	#move_and_collide(new_velocity)
+	#
+	##if velocity.x != 0 and velocity.y != 0:
+		##velocity.x /= 1.41421356
+		##velocity.y /= 1.41421356
+	#
